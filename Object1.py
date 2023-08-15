e084067c8fb2bf329d06ab3a844108112db31eaf
Herman Gahra <hermangahra48@gmail.com>
2023-08-15 11:54:34 -0600

    Created screen and object class

--- Object1.py
+++ Object1.py
@@ -1,3 +1,11 @@
+from main import *
 
-class Object1(): 
-    
+class Object1(py.sprite.Sprite): 
+    def __init__ (self, x, y):
+        body = py.Rect(100,100,100,100)
+        self.rect = self.body.get_rect()
+        self.width = 50
+        self.height = 50
+
+    def draw(self):
+        screen.blit()
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import pygame as py 
+from Object1 import *
+import pygame as py
 
 py.init()
-screen = py.display.set_mode((1280, 720))
 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+py.display.set_caption("Physics Simulator")
 running = True 
 
+clock = py.time.Clock()
+
 while running: 
+    
+    clock.tick(60)
 
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
 
-
+    screen.fill((255,255,255))
     py.display.flip()
     py.display.update()
 
