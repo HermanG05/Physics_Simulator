061902c6360c99c7c9d5f4043040618806acf0d8
Herman Gahra <hermangahra48@gmail.com>
2023-08-15 00:04:05 -0600

    Setup pygame

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame as py 
+
+py.init()
+screen = py.display.set_mode((1280, 720))
+
+running = True 
+
+while running: 
+
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+
+    py.display.flip()
+    py.display.update()
+
+py.QUIT()
+
